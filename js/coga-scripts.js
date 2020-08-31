322548a1c445c911bf72bc0798373c4280417305
Tom Woodward <twwoodward@me.com>
2020-08-31 11:59:22 -0400

    more tweaking for additional data viz

--- js/coga-scripts.js
+++ js/coga-scripts.js
@@ -48,8 +48,8 @@ function aud_monitoring(){
 
 function aud_peers(){
   if(document.getElementById('js-peers')){
-    const highM = document.getElementById('high-peers');
-    const lowM = document.getElementById('low-peers');
+    const highM = document.getElementById('high-peer');
+    const lowM = document.getElementById('low-peer');
 
     highM.addEventListener("click", showHighData);
     lowM.addEventListener("click", showLowData);
@@ -59,13 +59,13 @@ function aud_peers(){
       let genes = document.getElementById('peer-genes');
       let env = document.getElementById('peer-env');
       let details = document.getElementById('peer-data-details');
-      let holder = document.getElementById('data-holder');
-      genes.style.width = '20%';
+      let holder = document.getElementById('peer-data-holder');
+      genes.style.width = '60%';
       genes.style.opacity = '100%';
-      env.style.width = '60%';
+      env.style.width = '40%';
       env.style.opacity = '100%';
 
-      details.innerHTML = "<p>When parents pay close attention to their children’s whereabouts and activities, the environment is the most important factor influencing whether or not they are smoking or drinking.</p>";
+      details.innerHTML = "<p>When alcohol use among friends is high, genetic influences play the biggest role in why some adolescents are drinking more.</p>";
       lowM.classList.remove('active');
       lowM.classList.add('inactive');
       highM.classList.add('active');
@@ -74,14 +74,14 @@ function aud_peers(){
     }
 
     function showLowData(){
-      let genes = document.getElementById('genes');
-      let env = document.getElementById('env');
-      let details = document.getElementById('data-details')
-      genes.style.width = '70%';
+      let genes = document.getElementById('peer-genes');
+      let env = document.getElementById('peer-env');
+      let details = document.getElementById('peer-data-details')
+      genes.style.width = '20%';
       genes.style.opacity = '100%';
-      env.style.width = '20%';
+      env.style.width = '80%';
       env.style.opacity = '100%';
-      details.innerHTML = "<p>When parents don’t monitor their children closely, children’s genes are far more important in influencing whether or not they are smoking or drinking.</p>";
+      details.innerHTML = "<p>When alcohol use among friends is low, the environment is far more important in influencing risk.</p>";
        highM.classList.remove('active');
        highM.classList.add('inactive');
        lowM.classList.add('active');
