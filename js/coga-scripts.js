3a6d8fe054373d64c2c1fee6d8efd8d74c5340f1
heyohbunkbunk <mdroberts@vcu.edu>
2022-07-06 12:28:52 -0400

    Added 'back to top' button
    Added button to appear on scroll and take user to top via smooth scroll when clicked

--- js/coga-scripts.js
+++ js/coga-scripts.js
@@ -212,6 +212,28 @@ if (currentPath === '/coga/aud/genes-in-aud/') {
     document.querySelector('#aud-page-menu--flex li a[href="https://rampages.us/coga/aud/polygenic-risk-scores/"]').style.cssText += 'border-bottom: 2px solid #5f9cb4; font-weight: 700;';
 }
 
+//Button to return to top on AUD pages (or all)
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 //From https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
 // Select all links with hashes
