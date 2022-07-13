0d9148b5c36271394cab4a5a49ad66fe6c3d31ae
heyohbunkbunk <mdroberts@vcu.edu>
2022-07-13 16:46:40 -0400

    moved the dot

--- loop-templates/content-sidenotes.php
+++ loop-templates/content-sidenotes.php
@@ -22,16 +22,16 @@ defined( 'ABSPATH' ) || exit;
 	<aside id="aud-page-menu--flex">
 		<div class="aud-menu-title--flex">Alcohol Genetics Sections</div>
 		<ul>
-			<li><a href="https://rampages.us/coga/aud/genes-in-aud/" aria-label="Genes in AUD">• How do genes influence the risk for alcohol use disorders?</a></li>
-			<li><a href="https://rampages.us/coga/aud/alcohol-use-across-the-lifespan/" aria-label="Alcohol use across the lifespan">• Alcohol use across the lifespan</a></li>
-			<li><a href="https://rampages.us/coga/aud/heritability/" aria-label="Heritability">• Heritability</a></li>
-			<li><a href="https://rampages.us/coga/aud/genes-and-environment/" aria-label="Heritability">• Genes and Environment</a></li>
-			<li><a href="https://rampages.us/coga/aud/gene-identification/" aria-label="Gene Identification">• Gene Identification</a></li>
-			<li><a href="https://rampages.us/coga/aud/response-to-alcohol/" aria-label="Response to Alcohol">• Response to alcohol</a></li>
-			<li><a href="https://rampages.us/coga/aud/externalizing-alcohol-use-disorders/" aria-label="Externalizing Alcohol Use Disorders">• Externalizing and Alcohol Use Disorders</a></li>
-			<li><a href="https://rampages.us/coga/aud/internalizing-alcohol-use-disorders/" aria-label="Internalizing Alcohol Use Disorders">• Internalizing and Alcohol Use Disorders</a></li>
-			<li><a href="https://rampages.us/coga/aud/brain-function/" aria-label="Brain Function">• Brain Function</a></li>
-			<li><a href="https://rampages.us/coga/aud/polygenic-risk-scores/" aria-label="Polygenic Risk Scores">• Polygenic Risk Scores</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/genes-in-aud/" aria-label="Genes in AUD">How do genes influence the risk for alcohol use disorders?</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/alcohol-use-across-the-lifespan/" aria-label="Alcohol use across the lifespan">Alcohol use across the lifespan</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/heritability/" aria-label="Heritability">Heritability</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/genes-and-environment/" aria-label="Heritability">Genes and Environment</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/gene-identification/" aria-label="Gene Identification">Gene Identification</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/response-to-alcohol/" aria-label="Response to Alcohol">Response to alcohol</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/externalizing-alcohol-use-disorders/" aria-label="Externalizing Alcohol Use Disorders">Externalizing and Alcohol Use Disorders</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/internalizing-alcohol-use-disorders/" aria-label="Internalizing Alcohol Use Disorders">Internalizing and Alcohol Use Disorders</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/brain-function/" aria-label="Brain Function">Brain Function</a></li>
+			<li>• <a href="https://rampages.us/coga/aud/polygenic-risk-scores/" aria-label="Polygenic Risk Scores">Polygenic Risk Scores</a></li>
 		</ul>
 	</aside>
 	<div class="entry-content sidenotes-container">
